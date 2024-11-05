b7b2b0d8d591129f29b98b9cbeac2097ac5535ad
Dorina Laura Nagy <nagyneti2003@gmail.com>
2024-11-05 23:24:16 +0100

    szépítgetés

---
+++ TheWorld/TheWorld-master/TheWorld/TheWorld/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            World world = new World(10, 10);
+            bool running = true;
+
+            while (running)
+            {
+                Console.Clear();
+                world.Update();
+                world.Draw();
+
+                System.Threading.Thread.Sleep(100);
+
+                if (Console.KeyAvailable && Console.ReadKey(true).Key == ConsoleKey.Escape)
+                {
+                    running = false;
+                }
+            }
+
+            Console.WriteLine("A szimuláció leállt.");
+        }
+    }
+
+}
